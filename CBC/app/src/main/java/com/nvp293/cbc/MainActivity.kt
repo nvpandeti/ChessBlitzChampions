bc34df793a16de49ab53df433cd62b9fd0dfa134
Nikhil Pandeti <nikhil.pandeti@gmail.com>
2019-11-11 16:23:30 -0600

    Add email and google sign in options

--- CBC/app/src/main/java/com/nvp293/cbc/MainActivity.kt
+++ CBC/app/src/main/java/com/nvp293/cbc/MainActivity.kt
@@ -1,12 +1,84 @@
 package com.nvp293.cbc
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+//Facebook
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+
+        /*
+        setDisplayName.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            val displayName = displayNameET.text.toString()
+            val profileUpdates = UserProfileChangeRequest.Builder()
+                .setDisplayName(displayName)
+                .build()
+            user?.updateProfile(profileUpdates)
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("update", "User profile updated.")
+                        userTV.text = getDisplayString(user?.displayName, user?.email)
+                    }
+                }
+        }
+        */
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                userTV.text = getDisplayString(user?.displayName, user?.email)
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    fun getDisplayString(name : String?, email : String?) : String{
+        return "User: $name\nEmail: $email"
     }
 }
